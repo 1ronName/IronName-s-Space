932d09b2224d623fa1e4ba3c73a43e418b60bcc5
IronName <w526377yu@qq.com>
2025-08-16 14:04:50 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import "./styles/global.css";
 import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
