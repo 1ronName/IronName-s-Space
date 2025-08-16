b7c026d0279026e254c06f7cc8a339a919eb8c2f
IronName <w526377yu@qq.com>
2025-08-16 14:00:10 +0800

    Analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import App from "./App";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import "./styles/global.css";
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from "@vercel/analytics/next"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
       <ThemeProvider>
         <SpeedInsights/>
+        <Analytics />
         <App />
       </ThemeProvider>
     </HelmetProvider>
