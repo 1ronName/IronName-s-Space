27cb2726c6bd665e11780e6ba282a4364f3b1811
IronName <w526377yu@qq.com>
2025-08-20 20:25:37 +0800

    update

--- src/data/plans.ts
+++ src/data/plans.ts
@@ -5,16 +5,16 @@ export const plansData: Plan[] = [
     // 每次添加在这里，自然形成倒序
 
     {
+        id: 'blender',
+        title: 'Blender建模练习第一期',
+        description: '【Kurt】Blender零基础入门教程',
+        progress: 50
+    },
+    {
         id: 'art-v',
         title: '学画画',
         description: 'V大预科作业2.0',
         progress: 30
-    },
-    {
-        id: 'blender',
-        title: 'Blender建模',
-        description: '【Kurt】Blender零基础入门教程',
-        progress: 0
     },
     {
         id: 'story',
