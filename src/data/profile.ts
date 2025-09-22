cc1ea750cf3c980a0f320fa8b01a9baa41619f7e
IronName <w526377yu@qq.com>
2025-09-22 15:37:55 +0800

    avatar update

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -4,7 +4,7 @@ export const profileData: Profile = {
     name: 'IronName',
     title: '是一个一个大学生，对游戏开发感兴趣，还有其他有意思的东西。',
     bio: "目标是成为独立制作人！(大概不止游戏？",
-    avatar: 'images/avatar2.png', 
+    avatar: 'images/avatar3.png', 
     social: [
         {
             id: 'bilibili',
